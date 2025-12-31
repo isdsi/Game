631d4998e3ffbaa5bb3a94ca67d9387d7ac9fdac
isdsi <isdsi@hotmail.com>
2025-12-31 20:59:15 +0900

    d드라이브가 아닌 c드라이브 사용시 스크립트 적용, host.docker.internal 적용

--- GameServer/DockerDBRun.ps1
+++ GameServer/DockerDBRun.ps1
@@ -1,5 +1,6 @@
 # D 드라이브에 데이터 저장 폴더 생성
-New-Item -Path "D:\DockerData\postgres" -ItemType Directory -Force
+$DriveRoot = (Get-Location).Drive.Root
+New-Item -Path ($DriveRoot + "DockerData\postgres") -ItemType Directory -Force
 
 # PostgreSQL 컨테이너 실행
 # 수정된 실행 명령어 (도커 내부 경로를 /var/lib/postgresql 로 변경)
@@ -8,7 +9,7 @@ docker run -d `
   -e POSTGRES_USER=admin `
   -e POSTGRES_PASSWORD=password123 `
   -e POSTGRES_DB=game_server_db `
-  -v D:\DockerData\postgres:/var/lib/postgresql `
+  -v ($DriveRoot + "DockerData\postgres:/var/lib/postgresql") `
   -p 5432:5432 `
   postgres:18
   
