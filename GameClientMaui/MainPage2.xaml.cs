928b8f6b3ea4350d806478f14c5bb07690063fa6
isdsi <isdsi@hotmail.com>
2026-01-29 10:19:57 +0900

    Maui 클라이언트 작성중

---
+++ GameClientMaui/MainPage2.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GameClientMaui
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
