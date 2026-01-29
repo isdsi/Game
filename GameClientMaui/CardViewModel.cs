b9839c94d17c5d66eb6f0cf0b5dda08b1f74ef4c
isdsi <isdsi@hotmail.com>
2026-01-29 14:29:45 +0900

    SolitaireView/Model제거,GameClientPoco를 이용해서 Card 표시

--- GameClientMaui/CardViewModel.cs
+++ GameClientMaui/CardViewModel.cs
@@ -6,45 +6,34 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using GameClientPoco;
 
 namespace GameClientMaui
 {
-    public enum Suit { Spades, Hearts, Diamonds, Clubs }
-
     public class CardViewModel : INotifyPropertyChanged
     {
-        public Suit Suit { get; }
-        public int Rank { get; }
+        private Card _card;
 
-        private bool _isFaceUp;
+        public Card Card => _card;
+
+        public Suit Suit => _card.Suit;
+
+        public int Rank => _card.Rank;
+
+        public string CardString => _card.ToString();
+
         public bool IsFaceUp
         {
-            get => _isFaceUp;
-            set => SetProperty(ref _isFaceUp, value);
-        }
-
-        public string GetColor() => (Suit == Suit.Hearts || Suit == Suit.Diamonds) ? "Red" : "Black";
-
-        public override string ToString()
-        {
-            if (!IsFaceUp) return "[??]";
-            string r = Rank switch
+            get => _card.IsFaceUp;
+            set
             {
-                1 => "A",
-                11 => "J",
-                12 => "Q",
-                13 => "K",
-                _ => Rank.ToString()
-            };
-            char s = Suit switch
-            {
-                Suit.Spades => '♠',
-                Suit.Hearts => '♥',
-                Suit.Diamonds => '♦',
-                Suit.Clubs => '♣',
-                _ => ' '
-            };
-            return $"[{r}{s}]";
+                if (_card.IsFaceUp != value)
+                {
+                    _card.IsFaceUp = value; // 엔진의 값을 바꾸고
+                    OnPropertyChanged();      // UI에 알림
+                    OnPropertyChanged(nameof(CardString));
+                }
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -66,11 +55,10 @@ namespace GameClientMaui
 
         public ICommand FaceUpCommand { get; }
 
-        public CardViewModel(Suit suit, int rank)
+
+        public CardViewModel(Card card)
         {
-            Suit = suit;
-            Rank = rank;
-            _isFaceUp = false;
+            _card = card;
 
             FaceUpCommand = new RelayCommand(FaceUp);
         }
--- GameClientMaui/MainPage.xaml.cs
+++ GameClientMaui/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using GameClientPoco;
+using System.Collections.ObjectModel;
 
 namespace GameClientMaui
 {
@@ -12,10 +13,10 @@ namespace GameClientMaui
 
             Cards = new ObservableCollection<CardViewModel>
             {
-                new CardViewModel(Suit.Spades, 1),
-                new CardViewModel(Suit.Hearts, 12),
-                new CardViewModel(Suit.Diamonds, 7),
-                new CardViewModel(Suit.Clubs, 13)
+                new CardViewModel(new Card(Suit.Spades, 1)),
+                new CardViewModel(new Card(Suit.Hearts, 12)),
+                new CardViewModel(new Card(Suit.Diamonds, 7)),
+                new CardViewModel(new Card(Suit.Clubs, 13))
             };
             BindingContext = this;
         }
